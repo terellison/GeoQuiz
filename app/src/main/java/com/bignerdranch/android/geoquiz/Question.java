94ffad6314ecf835f3d10dc3554861ea75f63d57
Terry Ellison <ellisonterry4@gmail.com>
2017-03-22 23:58:49 -0400

    created Question class

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.geoquiz;
+
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
