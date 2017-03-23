42f1a70171e7d8f3775b4d4020444de37f8be266
Terry Ellison <ellisonterry4@gmail.com>
2017-03-23 01:11:01 -0400

    added Next button revised strings resource with new strings for questions

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -4,10 +4,30 @@ package com.bignerdranch.android.geoquiz;
 public class Question {
 
     private int mTextResId;
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
     private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
 
     public Question(int textResId, boolean answerTrue) {
         mTextResId = textResId;
+
         mAnswerTrue = answerTrue;
+
+
     }
 }
