6b4f846a303c856c189daae29edd17540b61999d
Terry Ellison <ellisonterry4@gmail.com>
2017-03-26 01:11:13 -0400

    added log statements for each subclass in QuizActivity.java

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,12 +2,15 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    public static final String TAG = "QuizActivity";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -43,8 +46,15 @@ public class QuizActivity extends AppCompatActivity {
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
     @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -75,6 +85,30 @@ public class QuizActivity extends AppCompatActivity {
 
         });
         updateQuestion();
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            Log.d(TAG, "onPause() called");
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            Log.d(TAG, "onResume() is called");
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            Log.d(TAG, "onStop() is called");
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            Log.d(TAG,"onDestroy() is called");
         }
     }
 
